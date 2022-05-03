cd56256d4246f72a356feab8ecaed265f2fbbfcd
longy <longhanzhang999@gmail.com>
2022-05-03 19:31:56 +0930

    First day of .net learning

---
+++ API/Data/Migrations/20220502151017_typosecondchanges.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class typosecondchanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PicturerUrl",
+                table: "Products",
+                newName: "PictureUrl");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PictureUrl",
+                table: "Products",
+                newName: "PicturerUrl");
+        }
+    }
+}
